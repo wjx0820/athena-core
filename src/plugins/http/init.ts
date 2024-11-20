b0a0c520efd1cd0af283ea7478176e1fef3b4800
yvbbrjdr <xyyqzy@hotmail.com>
2024-11-19 18:29:34 -0800

    http

---
+++ src/plugins/http/init.ts
@@ -0,0 +1,40 @@
+import { Athena } from "../../core/athena.js";
+import { PluginBase } from "../plugin-base.js";
+
+export default class Http extends PluginBase {
+  athena!: Athena;
+
+  desc() {
+    return null;
+  }
+
+  async load(athena: Athena) {
+    this.athena = athena;
+    athena.registerTool({
+      name: "http/fetch",
+      desc: "Fetches a URL.",
+      args: {
+        url: {
+          type: "string",
+          desc: "The URL to fetch.",
+          required: true,
+        },
+      },
+      retvals: {
+        result: {
+          type: "string",
+          desc: "The result of the fetch.",
+          required: true,
+        },
+      },
+      fn: async (args: { [key: string]: any }) => {
+        const response = await fetch(args.url);
+        return { result: await response.text() };
+      },
+    });
+  }
+
+  async unload() {
+    this.athena.deregisterTool("http/fetch");
+  }
+}
