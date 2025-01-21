62f6d4a0c2c7ce24d6dd5720395923393f6c1a5d
yvbbrjdr <xyyqzy@hotmail.com>
2025-01-20 22:45:47 -0800

    Async shell/exec

--- src/plugins/shell/init.ts
+++ src/plugins/shell/init.ts
@@ -2,9 +2,49 @@ import { exec } from "child_process";
 
 import { Athena, Dict } from "../../core/athena.js";
 import { PluginBase } from "../plugin-base.js";
+import { ShellProcess } from "./shell-process.js";
 
 export default class Shell extends PluginBase {
+  processes: { [key: number]: ShellProcess } = {};
+
   async load(athena: Athena) {
+    athena.registerEvent({
+      name: "shell/stdout",
+      desc: "Triggered when a shell command outputs to stdout",
+      args: {
+        pid: {
+          type: "number",
+          desc: "Process ID",
+          required: true,
+        },
+        stdout: {
+          type: "string",
+          desc: "Standard output of the command",
+          required: true,
+        },
+      },
+    });
+    athena.registerEvent({
+      name: "shell/terminated",
+      desc: "Triggered when a shell command terminates",
+      args: {
+        pid: {
+          type: "number",
+          desc: "Process ID",
+          required: true,
+        },
+        code: {
+          type: "number",
+          desc: "Exit code of the process",
+          required: true,
+        },
+        stdout: {
+          type: "string",
+          desc: "The remaining output of the process",
+          required: true,
+        },
+      },
+    });
     athena.registerTool({
       name: "shell/exec",
       desc: "Executes a shell command. Whenever you need to run a shell command, or the user's request requires running a shell command, use this tool.",
@@ -16,22 +56,92 @@ export default class Shell extends PluginBase {
         },
       },
       retvals: {
-        stdout: {
-          type: "string",
-          desc: "Standard output of the command",
+        pid: {
+          type: "number",
+          desc: "The running process ID",
           required: true,
         },
       },
-      fn: (args: Dict<any>) => {
-        return new Promise((resolve, reject) => {
-          exec(args.command, (error, stdout, stderr) => {
-            if (error) {
-              reject(Error(stderr));
-            } else {
-              resolve({ stdout });
-            }
+      fn: async (args: Dict<any>) => {
+        const process = new ShellProcess(args.command);
+        process.on("stdout", (data) => {
+          athena.emitEvent("shell/stdout", {
+            pid: process.pid,
+            stdout: data,
           });
         });
+        process.on("close", (code) => {
+          delete this.processes[process.pid];
+          athena.emitEvent("shell/terminated", {
+            pid: process.pid,
+            code,
+            stdout: process.stdout,
+          });
+        });
+        this.processes[process.pid] = process;
+        return { pid: process.pid };
+      },
+    });
+    athena.registerTool({
+      name: "shell/write-stdin",
+      desc: "Write string to stdin of the specified process.",
+      args: {
+        pid: {
+          type: "number",
+          desc: "Process ID",
+          required: true,
+        },
+        data: {
+          type: "string",
+          desc: "The data to write to stdin.",
+          required: true,
+        },
+      },
+      retvals: {
+        result: {
+          type: "string",
+          desc: "Result of the operation",
+          required: true,
+        },
+      },
+      fn: async (args: Dict<any>) => {
+        const process = this.processes[args.pid];
+        if (!process) {
+          throw new Error("Process not found");
+        }
+        process.write(args.data);
+        return { result: "success" };
+      },
+    });
+    athena.registerTool({
+      name: "shell/kill",
+      desc: "Kills a running shell command. Whenever you need to kill a running shell command, or the user's request requires killing a running shell command, use this tool.",
+      args: {
+        pid: {
+          type: "number",
+          desc: "Process ID",
+          required: true,
+        },
+        signal: {
+          type: "string",
+          desc: "Signal to send to the process",
+          required: false,
+        },
+      },
+      retvals: {
+        result: {
+          type: "string",
+          desc: "Result of the operation",
+          required: true,
+        },
+      },
+      fn: async (args: Dict<any>) => {
+        const process = this.processes[args.pid];
+        if (!process) {
+          throw new Error("Process not found");
+        }
+        process.kill(args.signal);
+        return { result: "success" };
       },
     });
     athena.registerTool({
@@ -67,6 +177,10 @@ export default class Shell extends PluginBase {
 
   async unload(athena: Athena) {
     athena.deregisterTool("shell/exec");
+    athena.deregisterTool("shell/write-stdin");
+    athena.deregisterTool("shell/kill");
     athena.deregisterTool("shell/apt-install");
+    athena.deregisterEvent("shell/stdout");
+    athena.deregisterEvent("shell/terminated");
   }
 }
---
+++ src/plugins/shell/shell-process.ts
@@ -0,0 +1,47 @@
+import { ChildProcess, spawn } from "child_process";
+import { EventEmitter } from "events";
+
+export class ShellProcess extends EventEmitter {
+  child: ChildProcess;
+  stdout: string = "";
+  stdoutTimeout?: NodeJS.Timeout;
+  boundStdoutHandler: (data: string) => void = this.stdoutHandler.bind(this);
+
+  constructor(command: string) {
+    super();
+    this.child = spawn(command, { shell: true });
+    this.child.stdout?.on("data", this.boundStdoutHandler);
+    this.child.stderr?.on("data", this.boundStdoutHandler);
+    this.child.once("close", (code) => {
+      if (this.stdoutTimeout) {
+        clearTimeout(this.stdoutTimeout);
+      }
+      this.child.stdout?.off("data", this.boundStdoutHandler);
+      this.child.stderr?.off("data", this.boundStdoutHandler);
+      this.emit("close", code);
+    });
+  }
+
+  get pid(): number {
+    return this.child.pid!;
+  }
+
+  write(data: string) {
+    this.child.stdin?.write(data);
+  }
+
+  kill(signal?: NodeJS.Signals) {
+    this.child.kill(signal);
+  }
+
+  stdoutHandler(data: string) {
+    this.stdout += data;
+    if (this.stdoutTimeout) {
+      clearTimeout(this.stdoutTimeout);
+    }
+    this.stdoutTimeout = setTimeout(() => {
+      this.emit("stdout", this.stdout);
+      this.stdout = "";
+    }, 2000);
+  }
+}
